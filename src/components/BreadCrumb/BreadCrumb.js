30a21e88dd5713758821686b04b4d01cb6440af0
unknown <sergiopiana@gmail.com>
2018-07-08 16:50:18 -0300

    fix

--- src/components/BreadCrumb/BreadCrumb.js
+++ src/components/BreadCrumb/BreadCrumb.js
@@ -9,7 +9,7 @@
 
 import React from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
-import s from './breadCrumb.css';
+import s from './BreadCrumb.css';
 
 class breadcrumb extends React.Component {
   render() {
